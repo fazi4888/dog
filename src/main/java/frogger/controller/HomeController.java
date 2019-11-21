a96a5c7883408c24ce6e90a9b5aea4b98b4a2c8b
Liqing YANG <slyly2@exmail.nottingham.ac.uk>
2019-11-21 15:52:46 +0000

    feat: add a simple START screen

---
+++ src/main/java/frogger/controller/HomeController.java
@@ -0,0 +1,27 @@
+package frogger.controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+
+public class HomeController {
+
+  @FXML
+  public void startGame() {
+
+  }
+
+  @FXML
+  public void showScoreboard() {
+
+  }
+
+  @FXML
+  public void exitGame() {
+    Platform.exit();
+  }
+
+  @FXML
+  public void switchMusicState() {
+
+  }
+}
