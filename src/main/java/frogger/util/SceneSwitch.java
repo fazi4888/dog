d896ae88f23cc65c47f853a80e4cca8d477e98d7
Liqing YANG <slyly2@exmail.nottingham.ac.uk>
2019-12-02 17:00:06 +0000

    feat: add game & selection screen

---
+++ src/main/java/frogger/util/SceneSwitch.java
@@ -0,0 +1,57 @@
+package frogger.util;
+
+import frogger.Main;
+import frogger.constant.GameLevel;
+import frogger.constant.GameMode;
+import frogger.controller.GameController;
+import frogger.model.Game;
+import frogger.model.World;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+public enum SceneSwitch {
+  INSTANCE;
+
+  private void changeScene(String fxmlFile) {
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+      Pane root = loader.load();
+      Scene scene = new Scene(root);
+      Main.getPrimaryStage().setScene(scene);
+      Main.getPrimaryStage().show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void switchToHome() {
+    changeScene("/frogger/view/home.fxml");
+  }
+
+  public void switchToSelection() {
+//    changeScene("/frogger/view/selection.fxml");
+  }
+
+  public void switchToGame(GameMode gameMode, GameLevel gameLevel) {
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("/frogger/view/game.fxml"));
+      Pane root = loader.load();
+      Scene scene = new Scene(root);
+      Main.getPrimaryStage().setScene(scene);
+
+      Game.INSTANCE.init(gameMode, gameLevel, root);
+      GameController gameController = loader.getController();
+      gameController.init(Game.INSTANCE);
+
+      scene.addEventHandler(KeyEvent.KEY_PRESSED, gameController::keyPressed);
+      scene.addEventHandler(KeyEvent.KEY_RELEASED, gameController::keyReleased);
+      Main.getPrimaryStage().show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
