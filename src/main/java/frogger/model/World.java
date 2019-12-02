fb8198c7b9199b603de3bbce1cee5ac46ce1221a
Liqing YANG <slyly2@exmail.nottingham.ac.uk>
2019-12-02 17:59:23 +0000

    refactor: rewrite class World

--- src/main/java/frogger/model/World.java
+++ src/main/java/frogger/model/World.java
@@ -1,109 +1,72 @@
 package frogger.model;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-import frogger.model.Actor;
-import javafx.animation.AnimationTimer;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.EventHandler;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyEvent;
+import frogger.constant.GameLevel;
+import frogger.constant.GameMode;
 import javafx.scene.layout.Pane;
 
+import java.util.Set;
 
-public abstract class World extends Pane {
-  private AnimationTimer timer;
+public class World {
 
-  public World() {
+  private Set<Log> logs;
+  private Set<Turtle> turtles;
+  private Set<WetTurtle> wetTurtles;
+  private Set<Car> cars;
 
-    sceneProperty().addListener(new ChangeListener<Scene>() {
+  private Set<End> ends;
 
-      @Override
-      public void changed(ObservableValue<? extends Scene> observable, Scene oldValue, Scene newValue) {
-        if (newValue != null) {
-          newValue.setOnKeyReleased(new EventHandler<KeyEvent>() {
+  private Frog frogA;
+  private Frog frogB;
 
-            @Override
-            public void handle(KeyEvent event) {
-              if (getOnKeyReleased() != null)
-                getOnKeyReleased().handle(event);
-              List<Actor> myActors = getObjects(Actor.class);
-              for (Actor anActor : myActors) {
-                if (anActor.getOnKeyReleased() != null) {
-                  anActor.getOnKeyReleased().handle(event);
-                }
-              }
-            }
-
-          });
-
-          newValue.setOnKeyPressed(new EventHandler<KeyEvent>() {
-
-            @Override
-            public void handle(KeyEvent event) {
-              if (getOnKeyPressed() != null)
-                getOnKeyPressed().handle(event);
-              List<Actor> myActors = getObjects(Actor.class);
-              for (Actor anActor : myActors) {
-                if (anActor.getOnKeyPressed() != null) {
-                  anActor.getOnKeyPressed().handle(event);
-                }
-              }
-            }
-
-          });
-        }
-
-      }
-
-    });
+  public World(GameMode gameMode) {
+    frogA = new Frog("/frogger/image/frog/froggerUp.png");
   }
 
-  public void createTimer() {
-    timer = new AnimationTimer() {
-      @Override
-      public void handle(long now) {
-        act(now);
-        List<Actor> actors = getObjects(Actor.class);
-
-        for (Actor anActor : actors) {
-          anActor.act(now);
-        }
-
-      }
-    };
+  public Frog getFrogA() {
+    return frogA;
   }
 
-  public void start() {
-    createTimer();
-    timer.start();
+  public Frog getFrogB() {
+    return frogB;
   }
 
-  public void stop() {
-    timer.stop();
+  private void load() {
+    // create a WorldReader class to read logs/turtles/wetTurtles/obstacles....
   }
 
-  public void add(Actor actor) {
-    getChildren().add(actor);
-  }
+  public void init(Pane root) {
+    //    load();
 
-  public void remove(Actor actor) {
-    getChildren().remove(actor);
-  }
+    root.getChildren().add(new Log("/frogger/image/log/log3.png", 170, 0, 222, 0.75));
+    root.getChildren().add(new Log("/frogger/image/log/log3.png", 170, 220, 222, 0.75));
+    root.getChildren().add(new Log("/frogger/image/log/log3.png", 170, 440, 222, 0.75));
+    root.getChildren().add(new Log("/frogger/image/log/log1.png", 340, 0, 358, -2));
+    root.getChildren().add(new Log("/frogger/image/log/log1.png", 340, 400, 358, -2));
+    root.getChildren().add(new Log("/frogger/image/log/log3.png", 170, 50, 425, 0.75));
+    root.getChildren().add(new Log("/frogger/image/log/log3.png", 170, 270, 425, 0.75));
+    root.getChildren().add(new Log("/frogger/image/log/log3.png", 170, 490, 425, 0.75));
 
-  public <A extends Actor> List<A> getObjects(Class<A> cls) {
-    ArrayList<A> someArray = new ArrayList<A>();
-    for (Node n : getChildren()) {
-      if (cls.isInstance(n)) {
-        someArray.add((A) n);
-      }
-    }
-    return someArray;
-  }
+    root.getChildren().add(new Turtle(500, 485, -1, 140));
+    root.getChildren().add(new Turtle(300, 485, -1, 140));
+    root.getChildren().add(new WetTurtle(700, 485, -1, 140, 140));
+    root.getChildren().add(new WetTurtle(600, 282, -1, 140, 140));
+    root.getChildren().add(new WetTurtle(400, 282, -1, 140, 140));
+    root.getChildren().add(new WetTurtle(200, 282, -1, 140, 140));
 
-  public abstract void act(long now);
+    root.getChildren().add(new Car("/frogger/image/car/truck1" + "Right.png", 0, 649, 1, 120, 120));
+    root.getChildren()
+        .add(new Car("/frogger/image/car/truck1" + "Right.png", 300, 649, 1, 120, 120));
+    root.getChildren()
+        .add(new Car("/frogger/image/car/truck1" + "Right.png", 600, 649, 1, 120, 120));
+    root.getChildren().add(new Car("/frogger/image/car/car1Left.png", 100, 597, -1, 50, 50));
+    root.getChildren().add(new Car("/frogger/image/car/car1Left.png", 250, 597, -1, 50, 50));
+    root.getChildren().add(new Car("/frogger/image/car/car1Left.png", 400, 597, -1, 50, 50));
+    root.getChildren().add(new Car("/frogger/image/car/car1Left.png", 550, 597, -1, 50, 50));
+    root.getChildren().add(new Car("/frogger/image/car/truck2Right.png", 0, 540, 1, 200, 200));
+    root.getChildren().add(new Car("/frogger/image/car/truck2Right.png", 500, 540, 1, 200, 200));
+    root.getChildren().add(new Car("/frogger/image/car/car1Left.png", 500, 490, -5, 50, 50));
+
+    root.getChildren().add(frogA);
+    //    root.getChildren().add(new Digit(0, 30, 360, 25));
+  }
 }
