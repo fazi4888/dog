5479ba6963e011caff9b3c2cdb2687c9e28fee3d
Liqing YANG <slyly2@exmail.nottingham.ac.uk>
2019-12-03 22:44:48 +0000

    feat: add interface Transformable & transform function for Turtle, WetTurtle

--- src/main/java/frogger/model/WetTurtle.java
+++ src/main/java/frogger/model/WetTurtle.java
@@ -2,53 +2,44 @@ package frogger.model;
 
 import javafx.scene.image.Image;
 
-public class WetTurtle extends Movable {
-  Image turtle1;
-  Image turtle2;
-  Image turtle3;
-  Image turtle4;
-  private int speed;
-  int i = 1;
-  boolean bool = true;
-  boolean sunk = false;
+import java.util.ArrayList;
 
-  @Override
-  public void act(long now) {
+public class WetTurtle extends AutomaticActor implements Transformable {
 
-    if (now / 900000000 % 4 == 0) {
-      setImage(turtle2);
-      sunk = false;
+  private boolean sunk = false;
+  private ArrayList<Image> turtleStates;
 
-    } else if (now / 900000000 % 4 == 1) {
-      setImage(turtle1);
-      sunk = false;
-    } else if (now / 900000000 % 4 == 2) {
-      setImage(turtle3);
-      sunk = false;
-    } else if (now / 900000000 % 4 == 3) {
-      setImage(turtle4);
-      sunk = true;
-    }
-
-    move(speed, 0);
-    if (getX() > 600 && speed > 0)
-      setX(-200);
-    if (getX() < -75 && speed < 0)
-      setX(600);
-  }
-
-  public WetTurtle(int xpos, int ypos, int s, int w, int h) {
-    turtle1 = new Image("/frogger/image/turtle/TurtleAnimation1.png", w, h, true, true);
-    turtle2 = new Image("/frogger/image/turtle/TurtleAnimation2Wet.png", w, h, true, true);
-    turtle3 = new Image("/frogger/image/turtle/TurtleAnimation3Wet.png", w, h, true, true);
-    turtle4 = new Image("/frogger/image/turtle/TurtleAnimation4Wet.png", w, h, true, true);
-    setX(xpos);
-    setY(ypos);
-    speed = s;
-    setImage(turtle2);
+  public WetTurtle(String imageLink, int xpos, int ypos, int width, double speed) {
+    super(imageLink, xpos, ypos, width, width, speed);
+    initTurtleState(width);
   }
 
   public boolean isSunk() {
     return sunk;
+  }
+
+  @Override
+  public void act(long now) {
+    super.act(now);
+    transform(now);
+  }
+
+  @Override
+  public void transform(long now) {
+    int stateIndex = (int) (now / 900000000 % 4);
+    setImage(turtleStates.get(stateIndex));
+    sunk = stateIndex == 3;
+  }
+
+  private void initTurtleState(int width) {
+    turtleStates =
+        new ArrayList<>() {
+          {
+            add(new Image("/frogger/image/turtle/TurtleAnimation2Wet.png", width, width, true, true));
+            add(new Image("/frogger/image/turtle/TurtleAnimation1.png", width, width, true, true));
+            add(new Image("/frogger/image/turtle/TurtleAnimation3Wet.png", width, width, true, true));
+            add(new Image("/frogger/image/turtle/TurtleAnimation4Wet.png", width, width, true, true));
+          }
+        };
   }
 }
