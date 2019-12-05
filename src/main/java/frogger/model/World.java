3c9cfb46581815144807bf945764763968597e0f
Liqing YANG <slyly2@exmail.nottingham.ac.uk>
2019-12-05 00:36:27 +0000

    feat: add class PreLoadActor to store pre-load actors of different level

--- src/main/java/frogger/model/World.java
+++ src/main/java/frogger/model/World.java
@@ -1,45 +1,21 @@
 package frogger.model;
 
 import frogger.util.WorldLoader;
-import javafx.scene.layout.Pane;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
 
 public class World {
 
   private Frog frogA;
   private Frog frogB;
-
-  private Set<Log> logs;
-  private Set<Turtle> turtles;
-  private Set<WetTurtle> wetTurtles;
-  private Set<Car> cars;
-
-  private Set<End> ends;
-
-  private Set<AutomaticActor> automaticActors;
+  private ArrayList<AutomaticActor> automaticActors;
+  private ArrayList<End> ends;
 
   public World(WorldLoader worldLoader) {
     frogA = worldLoader.getFrogA();
     frogB = worldLoader.getFrogB();
-
-    logs = worldLoader.getLogs();
-    turtles = worldLoader.getTurtles();
-    wetTurtles = worldLoader.getWetTurtles();
-    cars = worldLoader.getCars();
-
+    automaticActors = worldLoader.getAutomaticActors();
     ends = worldLoader.getEnds();
-
-    initAutomaticActor();
-  }
-
-  private void initAutomaticActor() {
-    automaticActors = new HashSet<>();
-    automaticActors.addAll(logs);
-    automaticActors.addAll(turtles);
-    automaticActors.addAll(wetTurtles);
-    automaticActors.addAll(cars);
   }
 
   public Frog getFrogA() {
