5f377b111fb34ba4040fd3b6859b71e536d0cd04
Liqing YANG <slyly2@exmail.nottingham.ac.uk>
2019-12-05 02:43:27 +0000

    refactor: add path of end image to FileName

--- src/main/java/frogger/constant/FileName.java
+++ src/main/java/frogger/constant/FileName.java
@@ -3,6 +3,9 @@ package frogger.constant;
 public class FileName {
   public static final String IMAGE_FROG_PREFIX = "/frogger/image/frog/frogger";
 
+  public static final String IMAGE_END = "/frogger/image/end/End.png";
+  public static final String IMAGE_END_FROG = "/frogger/image/end/FrogEnd.png";
+
   public static final String IMAGE_LOG_LONG = "/frogger/image/log/log1.png";
   public static final String IMAGE_LOG_MEDIUM = "/frogger/image/log/log2.png";
   public static final String IMAGE_LOG_SHORT = "/frogger/image/log/log3.png";
